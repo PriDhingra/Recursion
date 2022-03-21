edfe877c09fbf56e0912cecaead926aee9ca89d5
Prince Dhingra <princedhingra25@gmail.com>
2022-03-21 10:13:47 -0400

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int fact(int num) {
+    if(num == 1) 
+        return 1;
+    else    
+        return num * fact(num - 1);
+}
+
+int main() {
+    int num = 5;
+
+    cout<<"Factorial of "<<num<<" is "<<fact(num);
+
+    return 0;
+}
