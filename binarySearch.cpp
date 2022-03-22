8f07d92584dac3ea2b010225b7670b80e8e97829
Prince Dhingra <princedhingra25@gmail.com>
2022-03-21 23:58:11 -0400

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -2,25 +2,25 @@
 
 using namespace std;
 
-int binarySearch(int *arr, int start, int end, int size, int key) {
+int binarySearch(int *arr, int start, int end, int key) {
 
-    if(size == 0) 
+    if(start > end) 
         return -1;
 
     int mid = start + (end - start) / 2;
     if(arr[mid] == key) 
         return mid + 1;
     else if(arr[mid] > key) 
-        return binarySearch(arr, start, mid - 1, size, key);
+        return binarySearch(arr, start, mid - 1, key);
     else    
-        return binarySearch(arr, mid + 1, end, size, key);
+        return binarySearch(arr, mid + 1, end, key);
 
 }
 
 int main() {
     int arr[] = {1, 3, 6, 10, 12, 18};
 
-    cout<<"Element found at position "<<binarySearch(arr, 0, 5, 6, 3);
+    cout<<"Element found at position "<<binarySearch(arr, 0, 5, 3);
 
     return 0;
 }
