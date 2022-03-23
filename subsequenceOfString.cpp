38af9de51a31ad6db8cea7c7780a356dec7c75e6
Prince Dhingra <princedhingra25@gmail.com>
2022-03-23 13:15:09 -0400

    Create subsequenceOfString.cpp

---
+++ subsequenceOfString.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void powerSets(string str, string output, int index, vector<string> &result) {
+    if(index >= str.length()) {
+        result.push_back(output);
+        return;
+    }
+
+    powerSets(str, output, index + 1, result); 
+    output = output + str[index];
+    powerSets(str, output, index + 1, result);
+}
+
+int main() {
+    string str = "abcdef";
+
+    vector<string> result;
+    string output;
+    int index = 0;
+    powerSets(str, output, index, result);
+
+    for (int i = 0; i < result.size(); i++) {
+        cout<<result[i]<<endl;
+    }
+
+    return 0;
+}
